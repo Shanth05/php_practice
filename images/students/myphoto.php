eafc5980736d5d0518934c8d9e8677abba1567f0
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-04-21 16:23:39 +0530

    Update myphoto.php

--- images/students/myphoto.php
+++ images/students/myphoto.php
@@ -12,5 +12,7 @@ $img="../../page/md.jpg";
 
 <a href="../../page/myinfo.html"><img src="../../page/md.jpg"></a>
 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/h_R3FafQrU8?si=jkcf21b2s0ODJs2f" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 </body>
 </html>
