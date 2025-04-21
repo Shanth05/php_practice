96d15e98cba18d7cd9e84667ca706a965dd1de0b
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-04-21 16:23:44 +0530

    Create about.php

---
+++ page/about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<h1>Welcome to my about page!</h1>
+<nav>
+        <a href="../index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="video.php">Video</a>
+        <a href="contact.php" class="active">Contact</a>
+    </nav>
+</body>
+</html>
+
+
