2ac0625a9fdadbe818fe1e7e8b4e754a0fd312f2
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-04-21 16:23:46 +0530

    Create contact.php

---
+++ page/contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<h1>Welcome to my contact page!</h1>
+<nav>
+        <a href="../index.php">Home</a>
+        <a href="contact.php" class="active">Contact</a>
+        <a href="about.php">About</a>
+        <a href="video.php">Video</a>
+
+    </nav>
+<div style="position: relative;"><div style="position: relative; padding-bottom: 75%; height: 0; overflow: hidden;"><iframe style="position: absolute; top: 0; left: 0; width: 50%; height: 50%; border:0;" loading="lazy" allowfullscreen src="https://maps.google.com/maps?q=jaffna&output=embed"></iframe></div><a href="https://embeddablemap.com/" rel="noopener" target="_blank" style="position: absolute; width: 1px; height: 1px; padding: 0; margin: -1px; overflow: hidden; clip: rect(0,0,0,0); white-space: nowrap; border: 0;">embeddablemap.com</a></div>
+
+</body>
+</html>
+
