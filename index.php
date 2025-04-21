724bf27d752a36dd390b4d4143844e2b14807fd7
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-04-21 16:23:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,18 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php
-    echo "<h1> welcome to index page </h1>";
-
-    $pricee='30000';
-    $price=30000;
-    echo "$price";
-    var_dump($pricee);
-    var_dump($price);
-
-$name="priyanghan";
-echo "My name is:".$name;
-
-echo "<br>";
-
-echo "My name is $name";
-
-echo "<br>";
-
-echo 'My name is $name';
-
-echo "<br>";
-
-echo "My name is$name";
-
-echo "<br>";
-
-echo "$name is a student";
-
-echo "<br>";
-
-echo "{$name}is a student";
-    ?>
-
-  
+    <h1>Welcome to my website</h1>
+    <nav>
+        <a href="index.php" class="active">Home</a>
+        <a href="page/about.php">About</a>
+        <a href="page/video.php">Video</a>
+        <a href="page/contact.php">Contact</a>
+    </nav>
 </body>
 </html>
