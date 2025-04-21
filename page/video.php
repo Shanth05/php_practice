32207461cf62cd07174d983cb8780476f098246b
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-04-21 16:23:49 +0530

    Create video.php

---
+++ page/video.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+<h1>Welcome to my video page!</h1>
+    <nav>
+        <a href="../index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="video.php" class="active">Video</a>
+        <a href="contact.php">Contact</a>
+    </nav>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/xxyLkKYh78o?si=eFCnBEGsiwansxxb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+</body>
+</html>
